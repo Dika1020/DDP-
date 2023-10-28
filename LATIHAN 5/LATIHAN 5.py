a348c65007d1b1dacde6c72a3802a4e2bd54e48d
DKA_STARS <145969909+Dika1020@users.noreply.github.com>
2023-10-28 18:34:09 +0700

    Add files via upload
    TUGAS 5 DDP INDIKA PUTRA PRAISAH TI 06

---
+++ LATIHAN 5/LATIHAN 5.py
@@ -0,0 +1,27 @@
+a="kode kendaraan   :"
+b="nama kendaraan   :"
+c="merk kendaraan   :"
+d="jenis kendaraan  :"
+e="cc kendaraan     :"
+f="warna kendaraan  :"
+g="harga            :"
+h="jumlah ban       :"
+
+
+kendaraan =["B 203 TURU","ASTREA",100,"HITAM"]
+
+kendaraan.append("Rp.2.000.000")
+kendaraan.append(2)
+kendaraan.insert(2,"HONDA")
+kendaraan.insert(3,"BEBEK")
+
+print(a,kendaraan[0])
+print(b,kendaraan[1])
+print(c,kendaraan[2])
+print(d,kendaraan[3])
+print(e,kendaraan[4])
+print(f,kendaraan[5])
+print(g,kendaraan[6])
+print(h,kendaraan[7])
+
+
